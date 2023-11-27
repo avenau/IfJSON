14bf5abc9996eec1ee408ec4e9749a1f4b6999df
aven au <aven.au@fdmgroup.com>
2023-11-28 01:03:00 +1100

    Attempting to use composite pattern

--- src/main/java/com/avenau/ifjson/models/Condition.java
+++ src/main/java/com/avenau/ifjson/models/Condition.java
@@ -1,4 +1,13 @@
 package com.avenau.ifjson.models;
 
 public class Condition implements ConditionInterface{
+
+    private String type;
+    private String variable;
+    private String valueType;
+    private String value;
+    @Override
+    public boolean evaluate() {
+        return false;
+    }
 }
--- src/main/java/com/avenau/ifjson/models/ConditionInterface.java
+++ src/main/java/com/avenau/ifjson/models/ConditionInterface.java
@@ -1,4 +1,7 @@
 package com.avenau.ifjson.models;
 
 public interface ConditionInterface {
+
+    public boolean evaluate();
+
 }
